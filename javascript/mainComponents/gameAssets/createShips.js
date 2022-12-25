67229c535813c9f10fa050aa64697197b3d71212
Cem Mehmet Onal <mehmetcemonal@gmail.com>
2022-12-25 14:45:26 -0500

    new explode animation added / bugs fixed

--- javascript/mainComponents/gameAssets/createShips.js
+++ javascript/mainComponents/gameAssets/createShips.js
@@ -37,3 +37,49 @@ export const createShips= async(gameWave) =>
         return resolve({hero,enemies})
     })
 }
+
+
+
+function start()
+{
+    Array.from(document.querySelectorAll(".enemy")).forEach(arg => 
+        {
+            
+            const fragment = document.createDocumentFragment() // create and append new elements faster with method
+
+
+            const explodeAni = document.createElement("div");
+            explodeAni.id = 'ufo'
+
+
+
+            explodeAni.innerHTML= `
+            <div class="stick" style="--i:1"></div>
+            <div class="stick" style="--i:2"></div>
+            <div class="stick" style="--i:3"></div>
+            <div class="stick" style="--i:4"></div>
+            <div class="stick" style="--i:5"></div>
+            <div class="stick" style="--i:6"></div>
+            
+            <div id="circle"></div>
+            
+            <div class="light__wave"></div>
+
+            <div class="triangle" style="--j:1"></div>
+            <div class="triangle" style="--j:2"></div>
+            <div class="triangle" style="--j:3"></div>
+            <div class="triangle" style="--j:4"></div>
+            <div class="triangle" style="--j:5"></div>
+            <div class="triangle" style="--j:6"></div>
+            <div class="triangle" style="--j:7"></div>
+            <div class="triangle" style="--j:8"></div>
+                
+            `
+
+
+
+            fragment.appendChild(explodeAni)
+            arg.appendChild(fragment)
+        })
+
+}
--- javascript/mainComponents/gameAssets/shipClass.js
+++ javascript/mainComponents/gameAssets/shipClass.js
@@ -42,7 +42,50 @@ export class Ship
     {   
         if(document.querySelector(`#enemy${id}`))
         {
-         document.querySelector(`#enemy${id}`).style.filter= 'invert()';
+         //document.querySelector(`#enemy${id}`).style.filter= 'invert()';
+        
+
+            
+            const enemyToGo = document.querySelector(`#enemy${id}`)
+            const fragment = document.createDocumentFragment() // create and append new elements faster with method
+
+
+            const explodeAni = document.createElement("div");
+            explodeAni.id = 'ufo'
+
+
+
+            explodeAni.innerHTML= `
+            <div class="stick" style="--i:1"></div>
+            <div class="stick" style="--i:2"></div>
+            <div class="stick" style="--i:3"></div>
+            <div class="stick" style="--i:4"></div>
+            <div class="stick" style="--i:5"></div>
+            <div class="stick" style="--i:6"></div>
+            
+            <div id="circle"></div>
+            
+            <div class="light__wave"></div>
+
+            <div class="triangle" style="--j:1"></div>
+            <div class="triangle" style="--j:2"></div>
+            <div class="triangle" style="--j:3"></div>
+            <div class="triangle" style="--j:4"></div>
+            <div class="triangle" style="--j:5"></div>
+            <div class="triangle" style="--j:6"></div>
+            <div class="triangle" style="--j:7"></div>
+            <div class="triangle" style="--j:8"></div>
+                
+            `
+
+
+
+            fragment.appendChild(explodeAni)
+            enemyToGo.appendChild(fragment)
+
+
+
+
          setTimeout(() => {document.querySelector(`#enemy${id}`).remove()}, 2000);
         }
     }
